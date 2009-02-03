c163f4c4ade7e33063df6fb51e060f2df04f0e1c
CHRISTOPHER PEPLIN <peplin@caen-vnc01.engin.umich.edu>
2009-02-03 17:40:43 -0500

    Added request handler interface and public API

---
+++ workspace/Twoverse/src/twoverse/RequestHandlerServer.java
@@ -0,0 +1,68 @@
+package twoverse;
+
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.XmlRpcRequest;
+import org.apache.xmlrpc.common.XmlRpcHttpRequestConfig;
+import org.apache.xmlrpc.server.AbstractReflectiveHandlerMapping;
+import org.apache.xmlrpc.server.PropertyHandlerMapping;
+import org.apache.xmlrpc.server.XmlRpcHandlerMapping;
+import org.apache.xmlrpc.webserver.XmlRpcServlet;
+
+public class RequestHandlerServer extends XmlRpcServlet implements TwoversePublicApi { 
+    public RequestHandlerServer(ObjectManager objectManager,
+                            SessionManager sessionManager) {
+    }
+
+    public boolean login(String username, String password) {
+            return false;
+    }
+    
+    public boolean logout(String username, int session) {
+            return false;
+    }
+    
+    public void refreshUser(String username, int session) {
+            
+    }
+
+    // TODO double all of these, one accepts serialized object
+    public void addGalaxy() {
+
+    }
+
+    public void addPlanetarySystem() {
+
+    }
+
+    public void addManmadeBody() {
+    
+    }
+
+    public void changeName(int objectId) {
+
+    }
+
+    private boolean isAuthenticated(String username, String password) {
+        return false;
+    }
+	 
+    protected XmlRpcHandlerMapping newXmlRpcHandlerMapping()
+                                                    throws XmlRpcException {
+         PropertyHandlerMapping mapping
+             = (PropertyHandlerMapping) super.newXmlRpcHandlerMapping();
+         AbstractReflectiveHandlerMapping.AuthenticationHandler handler =
+             new AbstractReflectiveHandlerMapping.AuthenticationHandler(){
+                     public boolean isAuthorized(XmlRpcRequest pRequest){
+                         XmlRpcHttpRequestConfig config =
+                             (XmlRpcHttpRequestConfig) pRequest.getConfig();
+                         return isAuthenticated(config.getBasicUserName(),
+                             config.getBasicPassword());
+                     };
+             };
+        mapping.setAuthenticationHandler(handler);
+        return mapping;
+    }
+    
+    private ObjectManagerServer objectManager;
+    private SessionManager sessionManager;
+}
--- workspace/Twoverse/src/twoverse/TwoverseServer.java
+++ workspace/Twoverse/src/twoverse/TwoverseServer.java
@@ -4,22 +4,19 @@ import twoverse.util.Database;
 import twoverse.util.DatabaseException;
 
 public class TwoverseServer {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		try {
-			Database database = new Database();
-			ObjectManager objectManager = new ObjectManagerServer(database);
-			SessionManager sessionManager
-					= new SessionManager(database, objectManager);
-			SimulationRunner simulation = new SimulationRunner(objectManager);
-			
-		} catch (DatabaseException e) {
-			e.printStackTrace();
-		}
-
-	}
-
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        try {
+                Database database = new Database();
+                ObjectManager objectManager = new ObjectManagerServer(database);
+                SessionManager sessionManager
+                                = new SessionManager(database, objectManager);
+                SimulationRunner simulation = new SimulationRunner(objectManager);
+                
+        } catch (DatabaseException e) {
+                e.printStackTrace();
+        }
+    }
 }
